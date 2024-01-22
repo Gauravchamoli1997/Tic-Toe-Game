8c609386a79a2076d28afa89fb4157d03ee47706
Gaurav chamoli <gauravchamoli37@gmail.com>
2024-01-22 15:57:32 +0530

    second commit

--- app.js
+++ app.js
@@ -38,10 +38,11 @@ const resetGame = () => {
   turnO = true;
   enableBoxes();
   msgContainer.classList.add("hide");
+  showGameContainer();
 };
 
 const enableBoxes = () => {
-  for (box of boxes) {
+  for (let box of boxes) {
     box.disabled = false;
     box.innerText = "";
   }
@@ -58,18 +59,57 @@ const showWinner = (winner) => {
   msgContainer.classList.remove("hide");
   disableBoxes();
   highlightWinner();
+  hideGameContainer();
+};
+
+const showNoWinner = () => {
+  msg.innerText = "It's a tie! No one wins.";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+  hideGameContainer();
+};
+
+const hideGameContainer = () => {
+  const mainContainer = document.querySelector("main");
+  if (mainContainer) {
+    mainContainer.style.display = "none";
+  }
+};
+
+const showGameContainer = () => {
+  const mainContainer = document.querySelector("main");
+  if (mainContainer) {
+    mainContainer.style.display = "block";
+  }
 };
 
 const checkWinner = () => {
+  let winnerFound = false;
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+    if (pos1Val !=="" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
+        winnerFound = true;
+        break;
       }
+    }
+  }
+
+  if (!winnerFound) {
+    let allBoxesFilled = true;
+
+    for (let box of boxes) {
+      if (box.innerText === "") {
+        allBoxesFilled = false;
+        break;
+      }
+    }
+    if (allBoxesFilled) {
+      showNoWinner();
     }
   }
 };
